ca84d1ea02ac10e989200e9c050864cc81aa09aa
Marios Zindilis <marios@zindilis.com>
2014-07-20 05:41:31 +0300

    Changed variable name for sources directory.

--- z.py
+++ z.py
@@ -12,8 +12,8 @@ from string import Template
 
 # # Configurable options:
 z = {}
-## `opt_path_source`: the directory that contains the source files:
-z['opt_path_source']    = '/home/marios/Public/Dropbox/Code/zindilis.com'
+## `d_source`: the directory that contains the source files:
+d_source = '/home/marios/Public/Dropbox/Code/zindilis.com'
 ## `d_htdocs`: the directory served over HTTP:
 d_htdocs = '/home/marios/Public/Dropbox/Code/marios-zindilis.github.io'
 ## `opt_path_state`: the directory that contains the state files:
@@ -22,9 +22,9 @@ z['opt_path_state']     = '/home/marios/Public/Dropbox/Code/zalza/state'
 z['opt_path_build']	= '/home/marios/Public/Dropbox/Code/zalza/build'
 ## `opt_path_templates`: the directory containing the HTML templates:
 z['opt_path_templates'] = '/home/marios/Public/Dropbox/Code/zalza'
-## `skip_source_dirs`: subdirectories of `opt_path_source` to be ignored:
+## `skip_source_dirs`: subdirectories of `d_source` to be ignored:
 z['skip_source_dirs']	= ['.git', 'state']
-## `skip_source_files`: files in `opt_path_source` to be ignored:
+## `skip_source_files`: files in `d_source` to be ignored:
 z['skip_source_files'] 	= ['.gitignore', 'README.md']
 ## `generate_output`: whether or not the output will be generated
 z['generate_output']	= False
@@ -46,18 +46,18 @@ build = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 output_buffer = 'zBuild ' + build + '\n'
 output_buffer += '=' * len(output_buffer) + '\n\n'
 
-for traverse_root, traverse_dirs, traverse_files in os.walk(z['opt_path_source']):
+for traverse_root, traverse_dirs, traverse_files in os.walk(d_source):
 	for traverse_dir in traverse_dirs:
-		# Skip some subdirectories of `opt_path_source`:
-		if traverse_root == z['opt_path_source'] and traverse_dir in z['skip_source_dirs']:
+		# Skip some subdirectories of `d_source`:
+		if traverse_root == d_source and traverse_dir in z['skip_source_dirs']:
 			continue
 		# Also skip their subdirectories:
-		if traverse_root != z['opt_path_source'] and traverse_root[len(z['opt_path_source'])+1:].split('/')[0] in z['skip_source_dirs']:
+		if traverse_root != d_source and traverse_root[len(d_source)+1:].split('/')[0] in z['skip_source_dirs']:
 			continue
 
 		z['source_path'] = os.path.join(traverse_root, traverse_dir)
-		z['target_path'] = os.path.join(d_htdocs, z['source_path'][len(z['opt_path_source'])+1:])
-		z['state_path'] = os.path.join(z['opt_path_state'], z['source_path'][len(z['opt_path_source'])+1:])
+		z['target_path'] = os.path.join(d_htdocs, z['source_path'][len(d_source)+1:])
+		z['state_path'] = os.path.join(z['opt_path_state'], z['source_path'][len(d_source)+1:])
 
 		if not os.path.isdir(z['target_path']) or not os.path.isdir(z['state_path']):
 			z['generate_output'] = True
@@ -70,7 +70,7 @@ for traverse_root, traverse_dirs, traverse_files in os.walk(z['opt_path_source']
 			os.makedirs(z['target_path'])
 			output_buffer += '    Created its **target directory**:\n\n'
 			output_buffer += '        %s\n\n' % (z['target_path'])
-			os.chdir(z['opt_path_source'])
+			os.chdir(d_source)
 
 		if not os.path.isdir(z['state_path']):
 			os.makedirs(z['state_path'])
@@ -78,16 +78,16 @@ for traverse_root, traverse_dirs, traverse_files in os.walk(z['opt_path_source']
 			output_buffer += '        %s\n\n' % (z['state_path'])
 
 	for traverse_file in traverse_files:
-		# Skip some files in `opt_path_source`:
-		if traverse_root == z['opt_path_source'] and traverse_file in z['skip_source_files']:
+		# Skip some files in `d_source`:
+		if traverse_root == d_source and traverse_file in z['skip_source_files']:
 			continue 
-		# Also skip some subdirectories of `opt_path_source`:
-		if traverse_root != z['opt_path_source'] and traverse_root[len(z['opt_path_source'])+1:].split('/')[0] in z['skip_source_dirs']:
+		# Also skip some subdirectories of `d_source`:
+		if traverse_root != d_source and traverse_root[len(d_source)+1:].split('/')[0] in z['skip_source_dirs']:
 			continue
 
 		z['source_path'] = os.path.join(traverse_root, traverse_file)
 		z['source_content'] = file(z['source_path']).read()
-		z['state_path'] = os.path.join(z['opt_path_state'], z['source_path'][len(z['opt_path_source'])+1:]) + '.state'
+		z['state_path'] = os.path.join(z['opt_path_state'], z['source_path'][len(d_source)+1:]) + '.state'
 		
 		if not os.path.isfile(z['state_path']) \
 		or (os.path.isfile(z['state_path']) and file(z['state_path']).read() != hashlib.md5(z['source_content']).hexdigest()):
@@ -98,7 +98,7 @@ for traverse_root, traverse_dirs, traverse_files in os.walk(z['opt_path_source']
 			output_buffer += '        %s\n\n' % (z['source_path'])
 
 			if not z['source_path'].endswith('.md'):
-				z['target_path'] = os.path.join(d_htdocs, z['source_path'][len(z['opt_path_source'])+1:])
+				z['target_path'] = os.path.join(d_htdocs, z['source_path'][len(d_source)+1:])
 				shutil.copyfile(z['source_path'], z['target_path'])
 				output_buffer += '    Copied verbatim at:\n\n'
 				output_buffer += '        %s\n\n' % (z['target_path'])
@@ -125,9 +125,9 @@ for traverse_root, traverse_dirs, traverse_files in os.walk(z['opt_path_source']
 						break
 
 				z['page_title'] = z['site_name'] + ' - ' + headers['Title'] if headers.has_key('Title') else z['site_name']
-				z['page_section'] = traverse_root[len(z['opt_path_source'])+1:].split('/')[0]
-				z['target_path'] = os.path.join(d_htdocs, traverse_root[len(z['opt_path_source'])+1:], os.path.splitext(traverse_file)[0]) + '.html'
-				z['canonical_url'] = '/'.join([z['site_base_url'], (os.path.splitext(z['source_path'][len(z['opt_path_source'])+1:])[0] + '.html')])
+				z['page_section'] = traverse_root[len(d_source)+1:].split('/')[0]
+				z['target_path'] = os.path.join(d_htdocs, traverse_root[len(d_source)+1:], os.path.splitext(traverse_file)[0]) + '.html'
+				z['canonical_url'] = '/'.join([z['site_base_url'], (os.path.splitext(z['source_path'][len(d_source)+1:])[0] + '.html')])
 				target_content = Template(file(os.path.join(z['opt_path_templates'], 'tmpl_header.html')).read()).substitute(z)
 
 				pages_changed.append(z['canonical_url'])
@@ -174,8 +174,8 @@ for traverse_root, traverse_dirs, traverse_files in os.walk(z['opt_path_source']
 				output_buffer += '    Create state at:\n\n'
 				output_buffer += '        %s\n\n' % (z['state_path'])
 
-			os.chdir(z['opt_path_source'])
-			commit_path = z['source_path'][len(z['opt_path_source'])+1:]
+			os.chdir(d_source)
+			commit_path = z['source_path'][len(d_source)+1:]
 			p = subprocess.Popen(['git', 'add', commit_path])
 			p.communicate()
 			commit_mark = 'zBuild %s' % (datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S'))
