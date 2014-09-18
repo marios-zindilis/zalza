fb51aa10ce4c67a7be90889e0b6984419e09dbd1
Marios Zindilis <marios@zindilis.com>
2014-09-18 06:48:54 +0300

    Better readable syntax for building page_title.

--- z.py
+++ z.py
@@ -154,7 +154,9 @@ for traverse_root, traverse_dirs, traverse_files in os.walk(d_source):
 
                 tmpl = {}
                 tmpl['site_name'] = z['site_name']
-                tmpl['page_title'] = z['site_name'] + ' - ' + headers['Title'] if headers.has_key('Title') else z['site_name']
+                tmpl['page_title'] = z['site_name']
+                if headers.has_key('Title'):
+                    tmpl['page_title'] += ' - %s' % (headers['Title'])
                 target_path = os.path.join(d_htdocs, traverse_root[len(d_source)+1:], os.path.splitext(traverse_file)[0]) + '.html'
                 tmpl['canonical_url'] = '/'.join([z['site_base_url'], (os.path.splitext(source_subpath)[0] + '.html')])
                 target_content = Template(file(os.path.join(z['opt_path_templates'], 'tmpl_header.html')).read()).substitute(tmpl)
